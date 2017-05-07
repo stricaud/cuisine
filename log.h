ed717cceac37dd1c73498861499edcf106975f70
Sebastien Tricaud <sebastien@honeynet.org>
2017-05-06 18:00:39 -0700

    Add timestamp and fp redirection

--- log.h
+++ log.h
@@ -3,6 +3,7 @@
 
 #include <stdio.h>
 #include <stdarg.h>
+#include <time.h>
 
 #define LOG_LEVEL_DEBUG 1
 #define LOG_LEVEL_CRIT 2
@@ -12,33 +13,55 @@
 #define LOG_LEVEL_QUIET 6
 
 static int log_level = LOG_LEVEL_WARNING;
+static FILE *_fp = NULL; 
 
 void log_set_level(int level)
 {
   log_level = level;
 }
 
+void log_set_fp(FILE *fp)
+{
+  _fp = fp;
+}
+
+char *__get_time_now(void)
+{
+  time_t now;
+  struct tm *now_tm;
+  static char time_str[30];
+
+  now = time(NULL);
+  now_tm = localtime(&now);
+  strftime(time_str, sizeof(time_str), "%Y-%m-%d %H:%M:%S", now_tm);
+  return time_str;
+}
+
 void __log_func(int level, char *fmt, ...)
 {
   va_list ap;
-
+  time_t now;
+  
   if (level < log_level) return;
+  if (!_fp) { _fp = stderr; }
+  
+  fprintf(_fp, "[%s] ", __get_time_now());
   
   va_start(ap, fmt);
-  vfprintf(stderr, fmt, ap);
+  vfprintf(_fp, fmt, ap);
   va_end(ap);
 }
 
 
-#define log_debug(f, ...)			   \
+#define log_debug(f, ...)   \
   __log_func(LOG_LEVEL_DEBUG,   f, ##__VA_ARGS__)
-#define log_crit(f, ...)       			   \
+#define log_crit(f, ...)          \
   __log_func(LOG_LEVEL_CRIT,    f, ##__VA_ARGS__)
-#define log_error(f, ...)			   \
+#define log_error(f, ...)   \
   __log_func(LOG_LEVEL_ERROR,   f, ##__VA_ARGS__)
-#define log_warning(f, ...)			   \
+#define log_warning(f, ...)   \
   __log_func(LOG_LEVEL_WARNING, f, ##__VA_ARGS__)
-#define log_notice(f, ...)			   \
+#define log_notice(f, ...)   \
   __log_func(LOG_LEVEL_NOTICE,  f, ##__VA_ARGS__)
 
 void log_test()
@@ -49,5 +72,5 @@ void log_test()
   log_warning("warning\n");
   log_notice("notice\n");
 }
- 
+
 #endif // _LOG_H_
